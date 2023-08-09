5a92e1abf16f3d2aeeb93925744abf8354f1618f
suyunkai <82149368+suyunkai@users.noreply.github.com>
2023-08-09 13:24:00 +0800

    Update slice.ts

--- src/store/config/slice.ts
+++ src/store/config/slice.ts
@@ -1,6 +1,9 @@
 import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
 import { ChatGptConfig } from '@/types'
 import { NotificationInfo } from '@/types/admin'
+
+const VERSION = 'v2';
 
 export interface ConfigState {
   // 配置信息
@@ -23,13 +26,14 @@ export interface ConfigState {
   replaceData: (config: { [key: string]: any }) => void
 }
 
-const configStore = create<ConfigState>(
-  (set, get) => ({
-    configModal: false,
-    notifications: [],
-    shop_introduce: '',
-    user_introduce: '',
-    invite_introduce: '',
+const configStore = create<ConfigState>()(
+  persist(
+    (set, get) => ({
+      configModal: false,
+      notifications: [],
+      shop_introduce: '',
+      user_introduce: '',
+      invite_introduce: '',
       models: [
         {
           label: 'GPT-3.5',
@@ -52,28 +56,30 @@ const configStore = create<ConfigState>(
           value: 'gpt-4-32k'
         },
         {
-          label: 'GPT-4-备用',
-          value: 'gpt-4-beiyong'
-        },
-        {
           label: 'Claude 2',
           value: 'gpt-4-0613'
         },
       ],
-    config: {
-      model: 'gpt-3.5-turbo',
-      temperature: 0,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      max_tokens: 90000
-    },
-    setConfigModal: (value) => set({ configModal: value }),
-    changeConfig: (config) =>
-      set((state: ConfigState) => ({
-        config: { ...state.config, ...config }
-      })),
-    replaceData: (data) => set((state: ConfigState) => ({ ...state, ...data }))
-  })
+      config: {
+        model: 'gpt-3.5-turbo',
+        temperature: 0,
+        presence_penalty: 0,
+        frequency_penalty: 0,
+        max_tokens: 90000
+      },
+      setConfigModal: (value) => set({ configModal: value }),
+      changeConfig: (config) =>
+        set((state: ConfigState) => ({
+          config: { ...state.config, ...config }
+        })),
+      replaceData: (data) => set((state: ConfigState) => ({ ...state, ...data }))
+    }),
+    {
+      // 在存储键名中包括版本号
+      name: `config_storage_${VERSION}`, // 包括版本号
+      storage: createJSONStorage(() => localStorage) // (optional) by default the 'localStorage' is used
+    }
+  )
 )
 
 export default configStore
